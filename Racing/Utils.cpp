c36993dedd1e88c14c2bc596a9d23767c8105d69
maxisoft <maxisoft.eu@gmail.com>
2014-01-15 15:50:45 +0100

    use box2d utils

--- Racing/Utils.cpp
+++ Racing/Utils.cpp
@@ -14,10 +14,13 @@ Utils::~Utils()
 
 b2Vec2 Utils::RotateVect(const b2Vec2 vect, const float angle)
 {
-	float cosa = std::cosf(angle);
+	/*float cosa = std::cosf(angle);
 	float sina = std::sinf(angle);
 
-	return b2Vec2(cosa*vect.x - sina * vect.y, sina * vect.x + cosa * vect.y);
+	return b2Vec2(cosa*vect.x - sina * vect.y, sina * vect.x + cosa * vect.y);*/
+	
+	return b2Mul(b2Rot(angle), vect);
+
 }
 
 sf::Vector2f Utils::RotateVect(const sf::Vector2f vect, const float angle)
@@ -26,11 +29,6 @@ sf::Vector2f Utils::RotateVect(const sf::Vector2f vect, const float angle)
 	float sina = std::sinf(angle);
 
 	return sf::Vector2f(cosa* vect.x - sina * vect.y, sina * vect.x + cosa * vect.y);
-}
-
-b2Vec2 Utils::RotatePoint(const b2Vec2 vect, const float angle)
-{
-	return b2Vec2(vect.x * -std::sinf(angle), vect.y * std::cosf(angle));
 }
 
 
