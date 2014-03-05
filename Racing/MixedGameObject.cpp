76ea89c27ee95012d260ed698718504f09d1ab3f
maxisoft <maxisoft.eu@gmail.com>
2014-03-05 09:51:06 +0100

    fix : no more car shaking effect

--- Racing/MixedGameObject.cpp
+++ Racing/MixedGameObject.cpp
@@ -15,7 +15,10 @@ MixedGameObject::~MixedGameObject()
 void MixedGameObject::update(const float delta)
 {
 	auto position = getBody()->GetWorldCenter();
-	setSpritePosition(Utils::Box2DVectToSfVectPixel(position));
+	auto spritePosition = Utils::Box2DVectToSfVectPixel(position);
+	spritePosition.x = std::round(spritePosition.x);
+	spritePosition.y = std::round(spritePosition.y);
+	setSpritePosition(spritePosition);
 	auto angle = getBody()->GetAngle();
 	setSpriteRotation(Utils::RadianToDegree(angle));
 }
